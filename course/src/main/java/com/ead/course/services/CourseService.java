4db4a5498926ac99661e9f984ace344319b1e0ab
Rafael Sulimann <sulimannrafael@gmail.com>
2022-04-03 01:33:51 -0300

    Métodos Customizados em Repository com @Query, Deleção personalizada em CASCADE de Course e Module

--- course/src/main/java/com/ead/course/services/CourseService.java
+++ course/src/main/java/com/ead/course/services/CourseService.java
@@ -1,6 +1,15 @@
 package com.ead.course.services;
 
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import com.ead.course.models.CourseModel;
+import com.ead.course.models.LessonModel;
+import com.ead.course.models.ModuleModel;
 import com.ead.course.repositories.CourseRepository;
+import com.ead.course.repositories.LessonRepository;
+import com.ead.course.repositories.ModuleRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,6 +18,26 @@ import org.springframework.stereotype.Service;
 public class CourseService {
 
     @Autowired
-    private CourseRepository repository;
-    
+    private CourseRepository courseRepository;
+
+    @Autowired
+    private ModuleRepository moduleRepository;
+
+    @Autowired
+    private LessonRepository lessonRepository;
+
+    @Transactional
+    public void delete(CourseModel courseModel){
+        List<ModuleModel> moduleModelList = moduleRepository.findAllModulesIntoCourse(courseModel.getCourseId());
+        if(!moduleModelList.isEmpty()){
+            for(ModuleModel module : moduleModelList){
+                List<LessonModel> lessonModelList = lessonRepository.findAllLessonsIntoModule(module.getModuleId());
+                if(!lessonModelList.isEmpty()){
+                    lessonRepository.deleteAll(lessonModelList);
+                }
+            }
+            moduleRepository.deleteAll(moduleModelList);
+        }
+        courseRepository.delete(courseModel);
+    }    
 }
--- course/src/main/java/com/ead/course/services/ModuleService.java
+++ course/src/main/java/com/ead/course/services/ModuleService.java
@@ -1,5 +1,12 @@
 package com.ead.course.services;
 
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import com.ead.course.models.LessonModel;
+import com.ead.course.models.ModuleModel;
+import com.ead.course.repositories.LessonRepository;
 import com.ead.course.repositories.ModuleRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +16,17 @@ import org.springframework.stereotype.Service;
 public class ModuleService {
 
     @Autowired
-    private ModuleRepository repository;
-    
+    private ModuleRepository moduleRepository;
+
+    @Autowired
+    private LessonRepository lessonRepository;
+
+    @Transactional
+    public void delete(ModuleModel moduleModel){
+        List<LessonModel> lessonModelList = lessonRepository.findAllLessonsIntoModule(moduleModel.getModuleId());
+        if(!lessonModelList.isEmpty()){
+            lessonRepository.deleteAll(lessonModelList);
+        }
+        moduleRepository.delete(moduleModel);
+    }    
 }
