07fa0e8627c8e3239b3f166c9c1eeaa24dccedf6
Rafael Sulimann <sulimannrafael@gmail.com>
2022-06-30 17:48:37 -0300

    Delete Cascade com State Transfer Pattern

--- course/src/main/java/com/ead/course/repositories/CourseRepository.java
+++ course/src/main/java/com/ead/course/repositories/CourseRepository.java
@@ -21,4 +21,12 @@ public interface CourseRepository extends JpaRepository<CourseModel, UUID>, JpaS
     @Query(value = "insert into tb_courses_users values (:courseId, :userId)", nativeQuery = true)
     void saveCourseUser(@Param("courseId") UUID courseId, @Param("userId") UUID userId);
 
+    @Modifying
+    @Query(value = "delete FROM tb_courses_users WHERE course_id= :courseId", nativeQuery = true)
+    void deleteCourseUserByCourse(@Param("courseId") UUID courseId);
+
+    @Modifying
+    @Query(value = "delete FROM tb_courses_users WHERE user_id= :userId", nativeQuery = true)
+    void deleteCourseUserByUser(@Param("userId") UUID userId);
+
 }
--- course/src/main/java/com/ead/course/services/impl/UserServiceImpl.java
+++ course/src/main/java/com/ead/course/services/impl/UserServiceImpl.java
@@ -3,6 +3,8 @@ package com.ead.course.services.impl;
 import java.util.Optional;
 import java.util.UUID;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -10,6 +12,7 @@ import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import com.ead.course.models.UserModel;
+import com.ead.course.repositories.CourseRepository;
 import com.ead.course.repositories.UserRepository;
 import com.ead.course.services.UserService;
 
@@ -18,6 +21,9 @@ public class UserServiceImpl implements UserService{
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private CourseRepository courseRepository;
 
     public Page<UserModel> findAll(Specification<UserModel> spec, Pageable pageable){
         return userRepository.findAll(spec, pageable);
@@ -28,8 +34,10 @@ public class UserServiceImpl implements UserService{
         return userRepository.save(userModel);
     }
 
+    @Transactional
     @Override
     public void delete(UUID userId) {
+        courseRepository.deleteCourseUserByUser(userId);
         userRepository.deleteById(userId);
     }
 
