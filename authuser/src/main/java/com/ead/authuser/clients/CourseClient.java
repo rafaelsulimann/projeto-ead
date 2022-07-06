5e1ae8dd5c86268765ffdcde2d41adf92e599cf4
Rafael Sulimann <sulimannrafael@gmail.com>
2022-07-06 19:43:04 -0300

    Implementação de Circuit Breaker com Resilience4j

--- authuser/src/main/java/com/ead/authuser/clients/CourseClient.java
+++ authuser/src/main/java/com/ead/authuser/clients/CourseClient.java
@@ -3,10 +3,6 @@ package com.ead.authuser.clients;
 import java.util.List;
 import java.util.UUID;
 
-import com.ead.authuser.dtos.CourseDto;
-import com.ead.authuser.dtos.ResponsePageDto;
-import com.ead.authuser.services.UtilsService;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
@@ -18,6 +14,11 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
+import com.ead.authuser.dtos.CourseDto;
+import com.ead.authuser.dtos.ResponsePageDto;
+import com.ead.authuser.services.UtilsService;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
@@ -33,12 +34,14 @@ public class CourseClient {
     @Value("${ead.api.url.course}")
     public String REQUEST_URI_COURSE;
 
+    @CircuitBreaker(name = "circuitbreakerInstance")
     public Page<CourseDto> findAllCoursesByUser(UUID userId, Pageable pageable) {
         List<CourseDto> searchResult = null;
         ResponseEntity<ResponsePageDto<CourseDto>> result = null;
         String url = REQUEST_URI_COURSE + utilsService.createUrlGetAllCoursesByUser(userId, pageable);
         log.debug("Request URL {} ", url);
         log.info("Request URL {} ", url);
+        System.out.println("----STARTING COMUNICATION WITH COURSE----");
         try{
             ParameterizedTypeReference<ResponsePageDto<CourseDto>> responseType = new ParameterizedTypeReference<ResponsePageDto<CourseDto>>(){};
             result = restTemplate.exchange(url, HttpMethod.GET, null, responseType);
--- authuser/src/main/java/com/ead/authuser/configs/RestTemplateConfig.java
+++ authuser/src/main/java/com/ead/authuser/configs/RestTemplateConfig.java
@@ -1,5 +1,7 @@
 package com.ead.authuser.configs;
 
+import java.time.Duration;
+
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
@@ -9,11 +11,16 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class RestTemplateConfig {
 
+    private static final int TIMEOUT = 5000;
+
     @LoadBalanced
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
         // Do any additional configuration here
-        return builder.build();
+        return builder
+                .setConnectTimeout(Duration.ofMillis(TIMEOUT))
+                .setReadTimeout(Duration.ofMillis(TIMEOUT))
+                .build();
     }
     
 }
