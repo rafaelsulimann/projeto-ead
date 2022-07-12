dae5cd3d171ffa9297f37d72e8666e8098837018
Rafael Sulimann <sulimannrafael@gmail.com>
2022-07-12 04:03:24 -0300

    RabbitMqConfig em Notification, NotificationCommandDto, NotificationConsumer

---
+++ notification/src/main/java/com/ead/notification/dtos/NotificationCommandDto.java
@@ -0,0 +1,44 @@
+package com.ead.notification.dtos;
+
+import java.util.UUID;
+
+public class NotificationCommandDto {
+
+    private String title;
+    private String message;
+    private UUID userId;
+
+    public NotificationCommandDto(){
+    }
+
+    public NotificationCommandDto(String title, String message, UUID userId) {
+        this.title = title;
+        this.message = message;
+        this.userId = userId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UUID userId) {
+        this.userId = userId;
+    }    
+    
+}
